943899e7c41118fc8415376964554ca49f30dc71
valudollar <valerie.luxuanhui@gmail.com>
2023-07-23 20:59:23 +0800

    template

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+function Card() {
+  return (
+    <>
+      <div className="CardMain">
+        <div className="CardBody">
+          <div className="CardTop"></div>
+        </div>
+        <div className="CardPaper">
+          <p className="itemTitle">peer evaluation platform</p>
+        </div>
+        <div className="CardBottom"></div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
