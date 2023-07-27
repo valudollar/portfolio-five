7b4afba865e0f2d35ba66a30ff4c0a40e0234168
valudollar <valerie.luxuanhui@gmail.com>
2023-07-27 11:46:50 +0800

    background

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { motion } from "framer-motion";
 import Card from "./Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <hr className="topline"></hr>
@@ -33,19 +32,78 @@ function App() {
         </div>
       </div>
       <hr className="bottomline"></hr>
-      <div className="CardRow">
-        <Card title="peer evaluation platform"></Card>
-        <Card title="ride hailing simulation"></Card>
-        <Card title="handcarry baggage checker"></Card>
-        <Card title="deadlift form checker"></Card>
-        <Card title="prosthetics generative design"></Card>
-        <Card title="kueh lapis keyboard caps"></Card>
-        <Card title="bread basket keyboard caps"></Card>
-        <Card title="polymer clay keyboard caps"></Card>
-        <Card title="polymer clay jewellery"></Card>
-        <Card title="dinosaur quiz game"></Card>
-        <Card title="quest maker"></Card>
-      </div>
+      <section className="landing">
+        <div className="hello">
+          <p className="helloText">hello, valu here. </p>
+        </div>
+        <div className="transition">
+          <div className="transitionText">
+            <p>front-end development,</p>
+            <p>user-centric design</p>
+            <p>& illustration.</p>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="yellow"></div>
+        <div className="sectionContent">
+          <div className="left">
+            <div className="subtitleBox">
+              <div className="subtitleBot"></div>
+              <div className="subtitleTop">
+                {" "}
+                <p>projects</p>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="section"> */}
+          <div className="CardBox">
+            <Card
+              title="peer evaluation platform"
+              tags="bootstrap,css,javascript,reactjs,selenium,cucumber"
+            ></Card>
+            <Card
+              title="ride hailing simulation"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="handcarry baggage checker"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            {/* <Card
+              title="deadlift form checker"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="prosthetics generative design"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="kueh lapis keyboard caps"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="bread basket keyboard caps"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="polymer clay keyboard caps"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="polymer clay jewellery"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card
+              title="dinosaur quiz game"
+              tags="tag 1,tag 2,tag 3,tag 4"
+            ></Card>
+            <Card title="quest maker" tags="tag 1,tag 2,tag 3,tag 4"></Card> */}
+          </div>
+          {/* </div> */}
+        </div>
+      </section>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,36 @@
+import { useEffect } from "react";
+import { motion } from "framer-motion";
+
 function Card(props) {
+  let tags = props.tags.split(",");
+
   return (
     <>
       <div className="CardMain">
         <div className="CardBody">
           <div className="CardTop"></div>
+          <div className="CardBottom">
+            <div className="image"></div>
+            <div className="tagBox">
+              {tags.map((tag, id) => (
+                <div className="tag">
+                  <p className="tagTitle" key={id} value={tag}>
+                    {tag}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <p className=""> Mentored by Google</p>
+            <p className="description">
+              Project spanned across 14 weeks and 4 sprint cycles using Agile
+              Methology. ject spanned across 14 weeks and 4 sprint cycles using
+              Agile Methology. using Agile Methology
+            </p>
+          </div>
         </div>
         <div className="CardPaper">
           <p className="itemTitle">{props.title}</p>
         </div>
-        <div className="CardBottom"></div>
       </div>
     </>
   );
